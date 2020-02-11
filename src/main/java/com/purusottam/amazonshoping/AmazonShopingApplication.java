fee7bff1a5c75f65169296e35b2015c6908c7b0f
purusottam11 <ppurusottam1@gmail.com>
2020-02-11 13:28:42 +0530

    first commit

---
+++ src/main/java/com/purusottam/amazonshoping/AmazonShopingApplication.java
@@ -0,0 +1,13 @@
+package com.purusottam.amazonshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonShopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonShopingApplication.class, args);
+	}
+
+}
